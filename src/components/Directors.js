c49bae1817ca8dc502890bf74fd6b87c96d4ae8b
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-03 15:09:15 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
+const DirectorCard = (props) => {
+  const {name, movies} = props.director
+
   return (
     <div>
-      {/*{code here}*/}
+      <h3>{name}</h3>
+      <ul>{movies.map(movie => <li>{movie}</li> )}</ul>
     </div>
   );
+}
+
+const Directors = () => {
+  return directors.map( director => < DirectorCard key={directors.indexOf(director)} director={director} />) 
 }
 
 export default Directors
