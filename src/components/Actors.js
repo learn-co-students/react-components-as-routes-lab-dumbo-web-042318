30fa5d8a76e2080ec0deda15462399f838adbc45
cjunks94 <cjunks94@gmail.com>
2018-07-03 11:02:18 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,28 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Actors Page </h1>
+
+      {/*{code here}*/
+      actors.map((actor, index) =>{
+          return(
+            <div className='actorsPage' key={index}>
+            <h1>{actor.name}</h1>
+          <ul>
+            {
+              actor.movies.map((movie, index) =>{
+                return(
+                  <li key={index}>{movie}</li>
+                )
+              })
+            }
+          </ul>
+          </div>
+          )
+      })
+
+    }
+
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,26 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Directors Page </h1>
+
+      {/*{code here}*/
+      directors.map((director, index) =>{
+          return(
+            <div key={index}>
+            <h1>{director.name}</h1>
+          <ul>
+            {
+              director.movies.map((movie, index) =>{
+                return(
+                  <li key={index}>{movie}</li>
+                )
+              })
+            }
+          </ul>
+          </div>
+          )
+      })
+    }
     </div>
   );
 }
